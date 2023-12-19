1ef0857417e86ffcdb256ae9f953cf72bd35b582
Romi180400 <romiomer145@gmail.com>
2023-12-19 12:52:43 -0800

    first lesson

---
+++ mainmat.c
@@ -0,0 +1,10 @@
+#include "mymat.h"
+
+
+int main()
+{
+    mat MAT_A, MAT_B, MAT_C, MAT_D, MAT_E, MAT_F;
+    mat matrixs[] = {MAT_A, MAT_B, MAT_C, MAT_D, MAT_E, MAT_F};
+    intialize_mat(matrixs);
+    return 0;
+}
---
+++ mymat.h
@@ -0,0 +1,8 @@
+#define MAT_LENGTH 4
+
+typedef struct mat {
+    double table[MAT_LENGTH][MAT_LENGTH];
+    
+    }mat;
+
+mat A; 
